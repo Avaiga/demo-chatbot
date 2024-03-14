77d4c1b80f133baab0c1fcde30f0f07020736474
Alexandre Sajus <alexandre.sajus@student-cs.fr>
2024-03-14 14:16:25 +0100

    :bug: Input delay fix and adds title

--- main.py
+++ main.py
@@ -169,17 +169,17 @@ past_prompts = []
 
 page = """
 <|layout|columns=300px 1|
-<|part|render=True|class_name=sidebar|
+<|part|class_name=sidebar|
 # Taipy **Chat**{: .color-primary} # {: .logo-text}
 <|New Conversation|button|class_name=fullwidth plain|id=reset_app_button|on_action=reset_chat|>
 ### Previous activities ### {: .h5 .mt2 .mb-half}
 <|{selected_conv}|tree|lov={past_conversations}|class_name=past_prompts_list|multiple|adapter=tree_adapter|on_change=select_conv|>
 |>
 
-<|part|render=True|class_name=p2 align-item-bottom table|
-<|{conversation}|table|style=style_conv|show_all|width=100%|selected={selected_row}|rebuild|>
+<|part|class_name=p2 align-item-bottom table|
+<|{conversation}|table|style=style_conv|show_all|selected={selected_row}|rebuild|>
 <|part|class_name=card mt1|
-<|{current_user_message}|input|label=Write your message here...|on_action=send_message|class_name=fullwidth|>
+<|{current_user_message}|input|label=Write your message here...|on_action=send_message|class_name=fullwidth|change_delay=-1|>
 |>
 |>
 |>
@@ -197,4 +197,4 @@ if __name__ == "__main__":
 
     client = openai.Client(api_key=api_key)
 
-    Gui(page).run(debug=True, dark_mode=True, use_reloader=True)
+    Gui(page).run(debug=True, dark_mode=True, use_reloader=True, title="💬 Taipy Chat")
